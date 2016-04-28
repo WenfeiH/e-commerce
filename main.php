d12ca9bb1600b08e00aea795b259de64f9a67b1f
cjhjames89 <cjhjames89@gmail.com>
2016-04-27 21:27:59 -0700

    Adding images to the listing page

--- main.php
+++ main.php
@@ -34,31 +34,32 @@
                     $sql = "SELECT * FROM products"; 
                     foreach ($link->query($sql) as $row){
                         
-                        if ($count % 4 == 0){
-                            
-                        
-                    ?>
-                    
+                        if ($count % 4 == 0){     
+                ?>
                             <tr>
-<?php
+                <?php
                         }
-?>
+                        $name = explode(" ", $row['name']);
+                        if (count($name) == 2)
+                            $dir = $name[0]."_".$name[1];
+                        else
+                            $dir = $name[0]."_".$name[1]."_".$name[2];
+                        $img = "Images/".$dir."/".$dir.".jpg";
+                ?>
                         <td>
                             <div class="pic_cell">
-                                <a href=""><img alt="<?php echo $row['name'] ?> Image Is Not Available" src=""></a>
+                                <a href=""><img alt="<?php echo $row['name'] ?> Image Is Not Available" src="<?php echo $img ?>"></a>
                                 <div class="container">
-                                    <p class="name">Pokemon <?php echo $row['name'] ?></p>
+                                    <p class="name"><?php echo $row['name'] ?></p>
                                     <p class="rd">Release Date: <?php echo $row['releaseDate'] ?></p>
                                     <p class="gen">Generation: <?php echo $row['generation'] ?></p>
                                 </div>
                             </div>
                         </td>
                 
-<?php
-                    
-                        $count = $count + 1;     
-                        
-                    }
+                <?php
+                        $count = $count + 1;         
+                }
                 
 ?>
                 </tr>                
